7f5867f0b49a89ce6a6e13102ecaf1e2e61e550c
PatouBC <pbergez@outlook.fr>
2019-05-03 15:52:10 +0200

    gestion user

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,10 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this ->render('default/index.html.twig');
+        return $this ->render('default/index.html.twig',
+            ['base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR]);
     }
+    /**
+     * @Route("/admin", name="homeAdmin")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function indexAdmin()
+    {
+        return $this ->render('default/index.html.twig',
+            ['base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR]);
+    }
+
 }
